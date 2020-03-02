95b4fbbbfc611ad797527be516f7e1c2b3c8793b
guilhermealbm <gui.miranda.13@gmail.com>
2020-03-01 23:08:49 -0300

    get user location

---
+++ tim_maiapp/lib/UserLocation.dart
@@ -0,0 +1,33 @@
+import 'package:location/location.dart';
+import 'package:tuple/tuple.dart';
+
+class UserLocation {
+
+  static Future<Tuple2<String, String>> getUserLatAndLong() async{
+    Location location = new Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return new Tuple2("Couldn't get user's latitude", "Couldn't get user's longitude");
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.DENIED) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.GRANTED) {
+        return new Tuple2("Couldn't get user's latitude", "Couldn't get user's longitude");
+      }
+    }
+
+    _locationData = await location.getLocation();
+    return new Tuple2(_locationData.latitude.toString(), _locationData.longitude.toString());
+  }
+
+}
