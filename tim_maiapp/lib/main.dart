3f213eb7c96ddea5e07e7297d27596ceb35e095b
guilhermealbm <gui.miranda.13@gmail.com>
2020-03-14 17:30:54 -0300

    Adding mapbox_autocomplete to get user destination

--- tim_maiapp/lib/main.dart
+++ tim_maiapp/lib/main.dart
@@ -1,35 +1,74 @@
 import 'package:flutter/material.dart';
-import 'package:tuple/tuple.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_mapbox_autocomplete/flutter_mapbox_autocomplete.dart';
 import 'UserLocation.dart';
+import 'package:tuple/tuple.dart';
 
-void main() => runApp(TimMaiapp());
+Future main() async {
+  await DotEnv().load('.env');
+  runApp(TimMaiapp());
+}
 
 class TimMaiapp extends StatelessWidget {
-  // This widget is the root of your application.
-
   @override
   Widget build(BuildContext context) {
-    Tuple2 _location;
-    getUserLocation(_location);
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("TimMaiapp"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text("Your lat and long is:"),
-              Text(_location.item1),
-              Text(_location.item2)
-            ],
-          ),
+      title: "TimMaiapp",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _startPointController = TextEditingController();
+  Tuple2 _location;
+  @override
+  Widget build(BuildContext context) {
+    getCurrentUserLocation(_location);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TimMaiapp"),
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: CustomTextField(
+          hintText: "Enter destination",
+          textController: _startPointController,
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MapBoxAutoCompleteWidget(
+                  apiKey: DotEnv().env["map_box_token"],
+                  hint: "Enter destination",
+                  onSelect: (place) {
+                    print(place.center.elementAt(1));
+                    print(place.center.elementAt(0));
+                    print(place.toJson());
+                    _startPointController.text = place.placeName;
+                  },
+                  language: "pt",
+                  limit: 10,
+                  country: "BR",
+                ),
+              ),
+            );
+          },
+          enabled: true,
         ),
       ),
     );
   }
 
-  getUserLocation(Tuple2 _location) async => _location = await UserLocation.getUserLatAndLong();
-
+  getCurrentUserLocation(Tuple2 _location) async => _location = await UserLocation.getUserLatAndLong();
+  
 }
